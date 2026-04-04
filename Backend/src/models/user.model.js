a4042aa23a8d890d5f10ef6781b846d7e13bc79c
KushguptaPST <kushg0082@gmail.com>
2026-04-04 14:41:59 +0530

    add user model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ["user", "seller", "admin"],
+    default: "user"
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point"
+    },
+    coordinates: {
+      type: [Number],
+      default: [0, 0]
+    }
+  }
+});
+
+// Geo index
+userSchema.index({ location: "2dsphere" });
+
+export default mongoose.model("User", userSchema);
