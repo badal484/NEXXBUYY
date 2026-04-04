a5afd35f1a780c04823024c1253dda2855bb787a
badal484 <badal90603@gmail.com>
2026-04-04 13:26:46 +0530

    add imagekit logic

---
+++ Backend/src/config/imagekit.js
@@ -0,0 +1,17 @@
+import "../config/env.js";
+import ImageKit from "imagekit";
+
+const hasImageKitConfig =
+  process.env.IMAGEKIT_PUBLIC_KEY &&
+  process.env.IMAGEKIT_PRIVATE_KEY &&
+  process.env.IMAGEKIT_URL_ENDPOINT;
+
+const imagekit = hasImageKitConfig
+  ? new ImageKit({
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+    })
+  : null;
+
+export default imagekit;
