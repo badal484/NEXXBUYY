4e8032f4e02107bdfe10d7e917f5adce0c8966ab
badal484 <badal90603@gmail.com>
2026-04-04 13:28:27 +0530

    add get nearby product logic

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -57,3 +57,49 @@ export const addProduct = async (req, res) => {
     res.status(500).json({ msg: err.message });
   }
 };
+export const getNearbyProducts = async (req, res) => {
+  try {
+    const {lng,lat,minPrice,maxPrice, distance = 5000,} = req.query;
+
+
+    if (!lng || !lat) {
+      return res.status(400).json({ msg: "Location is required" });
+    }
+
+    const parsedLng = parseFloat(lng);
+    const parsedLat = parseFloat(lat);
+    const parsedDistance = parseInt(distance);
+
+    if (isNaN(parsedLng) || isNaN(parsedLat)) {
+      return res.status(400).json({ msg: "Invalid coordinates" });
+    }
+
+    const query = {
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [parsedLng, parsedLat],
+          },
+          $maxDistance: parsedDistance,
+        },
+      },
+    };
+
+    if (minPrice || maxPrice) {
+      query.price = {};
+
+      if (minPrice) query.price.$gte = parseInt(minPrice);
+      if (maxPrice) query.price.$lte = parseInt(maxPrice);
+    }
+
+    const products = await Product.find(query)
+      .populate("shop", "name address")
+      .populate("seller", "name email")
+      .sort({ createdAt: -1 }); 
+
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
