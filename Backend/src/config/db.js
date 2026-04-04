a07af016f604b59f114e937ed6a3b91b9f6c7e3c
badal484 <badal90603@gmail.com>
2026-04-04 13:25:34 +0530

    connect database logic

---
+++ Backend/src/config/db.js
@@ -0,0 +1,7 @@
+import "./env.js";
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB Connected");
+};
